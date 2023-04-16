4555c842920c7426e202545c7cbf56d28db33dcd
anthony chambon <acdf-ac.ccdf@hotmail.fr>
2023-04-16 12:17:51 +0200

    creation des entities et authentification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,26 @@
 import '../css/app.scss';
 
+import { Dropdown } from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdown
+    //new App();
+})
+
+const enableDropdown = () => {
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+    const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
+}
+
+
+
+
+// enableDropdown() {
+//     const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+//     dropdownElementList.map(function (dropdownToggleEl) {
+//         return new Dropdown(dropdownToggleEl);
+//     });}
+
+
 
